539a1eddb2e087d20d88493f1478ca553506eb0a
小青龙 <67395337+AzureDragon-Z@users.noreply.github.com>
2020-11-01 22:54:19 +0800

    Update 最大公约数与最小公倍数.cpp

--- 最大公约数与最小公倍数.cpp
+++ 最大公约数与最小公倍数.cpp
@@ -1,81 +1,14 @@
 #include <stdio.h>
- 
-void f(int &m,int &n){   //最小公倍数
-   if(m < n){
-    int c = m;
-    m = n;
-    n = c;
-  }
-  int k = 0;
-  for(int i = m;i > 0;i++)
-    if(i % m == 0 && i % n == 0){
-       k = i;
-       break;
-    }
-  printf("最小公倍数为:%d\n",k);
-} 
-
-void p(int m,int n){   //最大公约数
-  if(m < n){
-    int c = m;
-    m = n;
-    n = c;
-  }
-  int r = m % n;
-  while(r != 0){
-    m = n;
-    n = r;
-    r = m % n;
-  }
-  printf("最大公约数为:%d",n);
-}
-
 int main(){
-  int m,n;
-  printf("请输入要比较的两个数:");
-  scanf("%d %d",&m,&n);
-  f(m,n);
-  p(m,n);
-  return 0;
-}
-=======
-#include <stdio.h>
- 
-void f(int &m,int &n){   //最小公倍数
-   if(m < n){
-    int c = m;
-    m = n;
-    n = c;
-  }
-  int k = 0;
-  for(int i = m;i > 0;i++)
-    if(i % m == 0 && i % n == 0){
-       k = i;
-       break;
-    }
-  printf("最小公倍数为:%d\n",k);
-} 
-
-void p(int m,int n){   //最大公约数
-  if(m < n){
-    int c = m;
-    m = n;
-    n = c;
-  }
-  int r = m % n;
-  while(r != 0){
-    m = n;
-    n = r;
-    r = m % n;
-  }
-  printf("最大公约数为:%d",n);
-}
-
-int main(){
-  int m,n;
-  printf("请输入要比较的两个数:");
-  scanf("%d %d",&m,&n);
-  f(m,n);
-  p(m,n);
+  int a,b,r;
+  scanf("%d %d",&a,&b);
+  int m = a,n = b;
+  do {
+    r = a % b;
+    a = b;
+    b = r;
+  }while(r != 0);
+  printf("最大公约数为:%d",a);
+  printf("最大公倍数为：%d",m*n/a);
   return 0;
 }
